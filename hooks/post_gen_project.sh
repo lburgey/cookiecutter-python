697dc78f7e8423881bd0866f0809f87d21d0b135
Lukas Burgey <lukas.burgey@student.kit.edu>
2022-02-24 19:32:36 +0100

    Add instructions to post hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -7,3 +7,9 @@ git add -A
 git commit -m "Initial commit"
 git tag {{cookiecutter.version}}
 git remote add origin https://github.com/{{cookiecutter.github_username}}/{{cookiecutter.project_slug}}
+
+clear
+echo -e "\nProject {{cookiecutter.project_name}} created!"
+echo -e "\nPlease run tox to make sure everything is working:\n> tox"
+echo -e "\nIf you're missing tox run:\n> pip install -U tox"
+echo
